64f8e9b57cd2d5484ddece39ac80b26ac7af9521
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-01-16 00:59:43 +0200

    finished npm run scripts

--- express.sh
+++ express.sh
@@ -1,6 +1,29 @@
 #! /usr/local/bin/node
 
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+// Serve up public/ftp folder
+var serve = serveStatic('dist', {'index': ['index.html', 'index.htm']});
+
 // Create server
-require('http').createServer(function(req, res){
-  require('serve-static')('dist', {'index': ['index.html', 'index.htm']})(req, res, require('finalhandler')(req, res));
-}).listen(3000);
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+// Listen
+server.listen(3000);
+
+// // Create server
+// require('http').createServer(function(req, res){
+//   require('serve-static')(
+//       'dist',
+//       {'index': ['index.html', 'index.htm']}
+//   )(
+//       req,
+//       res,
+//       require('finalhandler')(req, res)
+//   );
+// }).listen(3000);
