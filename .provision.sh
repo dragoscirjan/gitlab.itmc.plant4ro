21feaafb2b4f49da9879171d10d1c2a531717c9e
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-01-15 22:43:48 +0200

    +

--- .provision.sh
+++ .provision.sh
@@ -9,4 +9,4 @@ npm install -g n && n latest
 rm -rf /vagrant/node_modules
 cd /vagrant && sudo -u vagrant -- npm install
 
-sed -e "s/localhost/window.location ? window.location.href : localhost"
+sed -e "s/localhost:/\" + (window.location ? window.location.href : 'localhost') + \":/" -i /vagrant/node_modules/live-reload/browser.js
