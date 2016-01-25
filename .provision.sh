8cbd64364868a73f5ed6e3a416ad28a80c8526a7
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-01-25 19:19:47 +0200

    +

--- .provision.sh
+++ .provision.sh
@@ -6,7 +6,10 @@ apt-get install -y npm git
 
 npm install -g n && n latest
 
+npm install -g jspm gulp
+
 rm -rf /vagrant/node_modules
 cd /vagrant && sudo -u vagrant -- npm install
+cd /vagrant && sudo -u vagrant -- jspm install
 
-sed -e "s/localhost:/\" + (window.location ? window.location.href : 'localhost') + \":/" -i /vagrant/node_modules/live-reload/browser.js
+# sed -e "s/localhost:/\" + (window.location ? window.location.href : 'localhost') + \":/" -i /vagrant/node_modules/live-reload/browser.js
