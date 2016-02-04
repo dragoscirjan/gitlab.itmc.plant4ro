885982257f48c07f1220683c8a249c8708fc24f0
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-02-05 00:17:18 +0200

    converting revolution with r.js

---
+++ services/bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+return array(
+    /**
+     *
+     */
+    '/copaci-pantati' => array(
+
+        /**
+         *
+         */
+        'get' => function() {
+            return time();
+        },
+    ),
+
+    /**
+     *
+     */
+    '/lista-donatori' => array(
+
+        /**
+         *
+         */
+        'get' => function() {
+
+        }
+    ),
+
+    /**
+     *
+     */
+    '/facebook' => array(
+        'get' => function() {
+            
+        }
+    ),
+
+);
