97449130ad4a9a03c06c1d8b986ed31e0474fe57
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-02-04 22:43:41 +0200

    +

--- src/com/acasa.js
+++ src/com/acasa.js
@@ -1,6 +1,6 @@
 //import {computedFrom} from 'aurelia-framework';
 
-export class Bunvenit {
+export class Acasa {
     heading = 'Planteaza pentru Romania';
 
     /**
--- src/com/app.js
+++ src/com/app.js
@@ -1,15 +1,15 @@
 export class App {
-  configureRouter(config, router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['', 'acasa'],   name: 'acasa',     moduleId: 'acasa',     nav: true, title: 'Acasa' },
-      { route: 'despre-proiect',  name: 'proiect',      moduleId: 'proiect',      nav: true, title: 'Despre Proiect' },
-      { route: 'parteneri',       name: 'parteneri',    moduleId: 'parteneri',    nav: true, title: 'Parteneri' },
-      { route: 'planteaza',       name: 'planteaza',    moduleId: 'planteaza',    nav: true, title: 'Planteaza' },
-      { route: 'contact',         name: 'contact',      moduleId: 'contact',      nav: true, title: 'Contact' }
-    //   { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
-    ]);
+    configureRouter(config, router) {
+        config.title = 'Planteaza pentru Romania';
+        config.map([
+            { route: ['', 'acasa'],     name: 'acasa',        moduleId: 'acasa',        nav: true, title: 'Acasa' },
+            { route: 'despre-proiect',  name: 'proiect',      moduleId: 'proiect',      nav: true, title: 'Despre Proiect' },
+            { route: 'parteneri',       name: 'parteneri',    moduleId: 'parteneri',    nav: true, title: 'Parteneri' },
+            { route: 'planteaza',       name: 'planteaza',    moduleId: 'planteaza',    nav: true, title: 'Planteaza' },
+            { route: 'contact',         name: 'contact',      moduleId: 'contact',      nav: true, title: 'Contact' }
+            //   { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+        ]);
 
-    this.router = router;
-  }
+        this.router = router;
+    }
 }
