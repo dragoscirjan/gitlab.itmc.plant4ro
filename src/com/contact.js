5113866cbed3a9bc20af56ec4bdf3a7ba8e6600f
dragoss <dragos.stefanache@itmediaconnect.ro>
2016-02-09 01:37:38 +0200

    +

--- src/com/contact.js
+++ src/com/contact.js
@@ -1,6 +1,14 @@
 //import {computedFrom} from 'aurelia-framework';
 
 export class Contact {
-  heading = 'Contact';
+    heading = 'Contact';
+
+    activate(params, routeConfig) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve.call(null);
+            }, 2000);
+        });
+    }
 
 }
