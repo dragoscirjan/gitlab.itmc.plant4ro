5a8a346c2b6f447459dc434ec8a6bc7d6b1c74a7
Dragos Cirjan <dragos.cirja@itmediaconnect.ro>
2016-07-28 22:06:05 +0300

    +

--- src/main.js
+++ src/main.js
@@ -10,22 +10,22 @@ export function configure(aurelia) {
     //     config.useLocale('ro-RO');//.useViewStrategy(ValidateCustomAttributeViewStrategy.TWBootstrapAppendToInput);
     // }*/)
     .plugin('google-recaptcha')
-    .plugin('aurelia-google-analytics', config => {
-        config.init('UA-642849-15');
-        config.attach({
-            logging: {
-                enabled: true, // Set to `true` to have some log messages appear in the browser console.
-            },
-            pageTracking: {
-                triggerEvent: 'router:navigation:success',
-                triggerCustomEvent: 'on:page:activate',
-                enabled: true // Set to `false` to disable in non-production environments.
-            },
-            clickTracking: {
-                enabled: true // Set to `false` to disable in non-production environments.
-            }
-        });
-    })
+    // .plugin('aurelia-google-analytics', config => {
+    //     config.init('UA-642849-15');
+    //     config.attach({
+    //         logging: {
+    //             enabled: true, // Set to `true` to have some log messages appear in the browser console.
+    //         },
+    //         pageTracking: {
+    //             triggerEvent: 'router:navigation:success',
+    //             triggerCustomEvent: 'on:page:activate',
+    //             enabled: true // Set to `false` to disable in non-production environments.
+    //         },
+    //         clickTracking: {
+    //             enabled: true // Set to `false` to disable in non-production environments.
+    //         }
+    //     });
+    // })
     //Uncomment the line below to enable animation.
     .plugin('aurelia-animator-css')
     //if the css animator is enabled, add swap-order="after" to all router-view elements
