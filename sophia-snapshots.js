83605a7fbd985f5b18a0969507cb31789f7de6fa
Dragos Cirjan <dragos.cirja@itmediaconnect.ro>
2016-07-28 21:38:58 +0300

    +

---
+++ sophia-snapshots.js
@@ -0,0 +1,28 @@
+
+
+const path = require('path');
+// const Sophia = require('sophia').Sophia;
+const Sophia = require(path.join(__dirname, '../itmcdev-sophia/index')).Sophia;
+const htmlSnapshots = require('html-snapshots');
+
+const options = {
+    match: /^http(s?):\/\/planteazapentruromania.ro/i,
+    maxDepth: 2,
+    indexMode: Sophia.INDEX_URL_MODE_QTREE,
+    selectors: {
+        __default: '.navbar-toggler:not(.spinner)'
+    }
+};
+
+Sophia.getInstance()
+    .indexUrls('https://planteazapentruromania.ro', options)
+    .then((urls) => {
+        console.log(url);
+        // var result = htmlSnapshots.run({
+        //     input: 'array',
+        //     source: urls,
+        //     outputDir: './snapshots',
+        //     outputDirClean: true,  
+        //     selector: options.selectors
+        // });
+    });
