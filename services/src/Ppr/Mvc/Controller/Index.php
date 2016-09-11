07211eae4e70719e056f0fab1e4185f5a26bd3c7
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2016-09-11 15:14:11 +0300

    +

--- services/src/Ppr/Mvc/Controller/Index.php
+++ services/src/Ppr/Mvc/Controller/Index.php
@@ -73,7 +73,7 @@ class Index {
                     'error' => 'Failed grabbing tree count',
                     'message' => $e->getMessage(),
                     'trace' => $e->getTraceAsString(),
-            ));
+            ), $app);
         }
     }
 
